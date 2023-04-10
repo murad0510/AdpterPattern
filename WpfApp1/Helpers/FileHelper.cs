9eff06cc4f3c3cd6ab099912988f923ec712e711
Azizo_pu01 <Azizo_pu01@STHQ0127-03>
2023-04-10 16:18:27 +0400

    Add project files.

---
+++ WpfApp1/Helpers/FileHelper.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WpfApp1.Helpers
+{
+    internal class FileHelper
+    {
+        public static void WriteBooks(Human human)
+        {
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter($"{human.Name}.json"))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    serializer.Serialize(jw, human);
+                }
+            }
+        }
+
+        public static List<Human> ReadStudents()
+        {
+            List<Human> students = null;
+            var serializer = new JsonSerializer();
+            using (var sr = new StreamReader("students.json"))
+            {
+                using (var jr = new JsonTextReader(sr))
+                {
+                    students = serializer.Deserialize<List<Human>>(jr);
+                }
+            }
+            return students;
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/MainWindowViewModel/Main.cs
@@ -0,0 +1,30 @@
+﻿using CinemaPlus.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1.ViewModel.MainWindowViewModel
+{
+
+    public class Main
+    {
+        public RelayCommand MyJsonRelayCommand { get; set; }
+        public RelayCommand MyXmlRelayCommand { get; set; }
+
+        public Main()
+        {
+            MyJsonRelayCommand = new RelayCommand((a) =>
+            {
+                MessageBox.Show("json");
+            });
+
+            MyXmlRelayCommand = new RelayCommand((a) =>
+            {
+                MessageBox.Show("Xml");
+            });
+        }
+    }
+}
